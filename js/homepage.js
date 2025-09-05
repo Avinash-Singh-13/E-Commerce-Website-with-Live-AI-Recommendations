98f4a40bef2fc6bcf4d4d86ffd2be5822f31c2d1
Avinash Singh <2k22.cse.2211369@gmail.com>
2025-09-05 10:42:24 +0530

    Update homepage.js

--- js/homepage.js
+++ js/homepage.js
@@ -17,7 +17,7 @@ function redirectToPersonalDetails() {
   window.location.href = "../pages/personal_details.html";
 }
 
-// Redirect to Recommended Page
+// Redirect to Recommended Page 
 function redirectToRecommended() {
   // Retrieve the input values for username and password from the HTML form
   const username = document.getElementById("username").value;
